067c63fb423056c84ea8636db4d3d1bc098094d0
19Nazar <vballiev@gmail.com>
2024-01-18 00:15:44 +0200

    fix: vercel fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { MintbaseWalletContextProvider } from "@mintbase-js/react";
+// import { MintbaseWalletContextProvider } from "@mintbase-js/react";
 import "./App.css";
-import { NearWalletConnector } from "./NearWalletConnector";
-import "./shims";
+// import { NearWalletConnector } from "./NearWalletConnector";
+// import "./shims";
 
 function App() {
 
-  const MintbaseWalletSetup = {
-    contractAddress: "hellovirtualworld.mintspace2.testnet",
-    network: "testnet",
-    callbackUrl: "http://localhost:3000",
-  };
+  // const MintbaseWalletSetup = {
+  //   contractAddress: "hellovirtualworld.mintspace2.testnet",
+  //   network: "testnet",
+  //   callbackUrl: "http://localhost:3000",
+  // };
 
   return (
     <div>

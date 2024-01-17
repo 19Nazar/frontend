407eb829140450b12e33e802e84c0b0d3691e044
19Nazar <vballiev@gmail.com>
2024-01-17 23:08:35 +0200

    add: add file react-vite

---
+++ react-vite/src/App.tsx
@@ -0,0 +1,21 @@
+import { MintbaseWalletContextProvider } from "@mintbase-js/react";
+import "./App.css";
+import { NearWalletConnector } from "./NearWalletConnector";
+import "./shims";
+
+function App() {
+
+  const MintbaseWalletSetup = {
+    contractAddress: "hellovirtualworld.mintspace2.testnet",
+    network: "testnet",
+    callbackUrl: "http://localhost:3000",
+  };
+
+  return (
+<div>
+  <h1>Hello world</h1>
+</div>
+  );
+}
+
+export default App;
